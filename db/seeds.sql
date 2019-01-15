58327457aab9d449bba1ff92c1d3c38fee7c4b56
Carlos M Garcia <cgarcia101015@gmail.com>
2019-01-15 00:28:40 -0500

    upto controller setup

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers (burger_name, devoured) values ('Avocado Burger', false);
+
+INSERT INTO burgers (burger_name, devoured) values ('Turkey Burger', false);
+
+INSERT INTO burgers (burger_name, devoured) values ('Veggie Burger', false);
+
+INSERT INTO burgers (burger_name, devoured) values ('Bacon Cheeseburger', false);
+
+SELECT * FROM burgers;
